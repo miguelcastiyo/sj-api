6ee180be492b32df36a8ca7c8aab1e7585dff274
miguel <miguelc91711@gmail.com>
2025-04-27 23:22:35 -0700

    added cors file

--- api/v1/auth/login.php
+++ api/v1/auth/login.php
@@ -1,6 +1,10 @@
 <?php
 require_once __DIR__ . '/../../../lib/db.php';
 require_once __DIR__ . '/../../../lib/session.php';
+require_once __DIR__ . '/../../../lib/cors.php';
+
+$raw = file_get_contents('php://input');
+error_log("🔍 Raw incoming data: " . $raw);
 
 header('Content-Type: application/json');
 
